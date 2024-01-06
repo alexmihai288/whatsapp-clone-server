b62ac319b8e1da364fd72463f3a6e427b3f344b9
Moldovan Alexis <moldovanalexismihai06@gmail.com>
2024-01-06 22:53:33 +0200

    manage user and livekit to be done

--- dist/index.js/index.js
+++ dist/index.js/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,6 +16,7 @@ const express_1 = __importDefault(require("express"));
 const node_http_1 = require("node:http");
 const socket_io_1 = require("socket.io");
 const cors_1 = __importDefault(require("cors")); // Import the cors middleware
+const db_1 = require("./db");
 const app = (0, express_1.default)();
 const server = (0, node_http_1.createServer)(app);
 const io = new socket_io_1.Server(server, {
@@ -18,9 +28,18 @@ app.use((0, cors_1.default)());
 app.get("/", (req, res) => {
     res.send("<h1>Hello world</h1>");
 });
-io.on("connection", (socket) => {
+io.on("connection", (socket) => __awaiter(void 0, void 0, void 0, function* () {
     console.log("a user connected");
     socket.join(socket.handshake.query.connectionId);
+    const currentUserGroups = yield db_1.db.profile.findFirst({
+        where: {
+            connectionId: socket.handshake.query.connectionId,
+        },
+        include: {
+            groupsMember: true,
+        },
+    });
+    currentUserGroups === null || currentUserGroups === void 0 ? void 0 : currentUserGroups.groupsMember.map((group) => socket.join(group.groupId));
     socket.on("send-message", (value, fileUrl, currentMemberId, toMemberId, conversationId) => {
         socket
             .to(toMemberId)
@@ -29,7 +48,26 @@ io.on("connection", (socket) => {
     socket.on("new-message-settled", (toMemberId) => {
         socket.to(toMemberId).emit("receive-message-settled");
     });
-});
+    //groups
+    socket.on("join-group", (groupId) => {
+        socket.join(groupId);
+    });
+    socket.on("invite-to-group", (memberId, groupId) => {
+        socket.to(memberId).emit("receive-invite-to-group", groupId);
+    });
+    socket.on("send-group-message", (value, fileUrl, currentMemberId, groupId) => {
+        socket
+            .to(groupId)
+            .emit("receive-group-message", value, fileUrl, currentMemberId, groupId);
+    });
+    socket.on("invalidate-invite-members", (ownerId) => {
+        console.log(ownerId);
+        socket.to(ownerId).emit("receive-invalidate-invite-members");
+    });
+    socket.on("new-group-message-settled", (groupId) => {
+        socket.to(groupId).emit("receive-group-message-settled");
+    });
+}));
 server.listen(5000, () => {
     console.log("server running at http://localhost:5000");
 });
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
 import cors from "cors"; // Import the cors middleware
+import { db } from "./db";
 
 const app = express();
 const server = createServer(app);
@@ -16,10 +17,22 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
-io.on("connection", (socket) => {
+io.on("connection", async (socket) => {
   console.log("a user connected");
 
   socket.join(socket.handshake.query.connectionId!);
+
+  const currentUserGroups = await db.profile.findFirst({
+    where: {
+      connectionId: socket.handshake.query.connectionId as string,
+    },
+    include: {
+      groupsMember: true,
+    },
+  });
+
+  currentUserGroups?.groupsMember.map((group) => socket.join(group.groupId));
+
   socket.on(
     "send-message",
     (value, fileUrl, currentMemberId, toMemberId, conversationId) => {
@@ -36,6 +49,39 @@ io.on("connection", (socket) => {
   );
   socket.on("new-message-settled", (toMemberId) => {
     socket.to(toMemberId).emit("receive-message-settled");
+  });
+
+  //groups
+  socket.on("join-group", (groupId) => {
+    socket.join(groupId);
+  });
+
+  socket.on("invite-to-group", (memberId, groupId) => {
+   socket.to(memberId).emit("receive-invite-to-group", groupId);
+  });
+
+  socket.on(
+    "send-group-message",
+    (value, fileUrl, currentMemberId, groupId) => {
+      socket
+        .to(groupId)
+        .emit(
+          "receive-group-message",
+          value,
+          fileUrl,
+          currentMemberId,
+          groupId
+        );
+    }
+  );
+
+    socket.on("invalidate-invite-members",(ownerId)=>{
+      console.log(ownerId)
+      socket.to(ownerId).emit("receive-invalidate-invite-members")
+    })
+
+  socket.on("new-group-message-settled", (groupId) => {
+    socket.to(groupId).emit("receive-group-message-settled");
   });
 });
 
